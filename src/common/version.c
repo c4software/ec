875935aed16dd926d5a7c47c9cccb910b4eca2d2
Tim Crawford <tcrawford@system76.com>
2020-10-21 17:52:06 -0600

    Add magic strings read by uecflash
    uecflash, the program used to flash the EC from firmware-update,
    expects the binary to have specfic embedded strings in order to report
    the EC project, version, and date.

--- src/common/version.c
+++ src/common/version.c
@@ -22,3 +22,8 @@ const char * board() {
 const char * version() {
     return &VERSION[13];
 }
+
+// Magic strings read by uecflash
+static const char __code UECFLASH_PROJECT[] = "PRJ:" xstr(__BOARD__) "$";
+static const char __code UECFLASH_VERSION[] = "VER: 1.0.0$";
+static const char __code UECFLASH_DATE[] = "DATE: " xstr(__FIRMWARE_DATE__) "$";
